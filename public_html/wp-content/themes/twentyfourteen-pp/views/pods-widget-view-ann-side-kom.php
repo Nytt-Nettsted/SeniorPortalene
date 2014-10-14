6e1d73ec2468ae4881b659f7c9e757ee2c4518e0
Knut Sparhelll <knut@sparhell.no>
2014-10-14 13:38:08 +0200

    Annonser
    Annonser på fylke vises for kommune.

--- public_html/wp-content/themes/twentyfourteen-pp/views/pods-widget-view-ann-side-kom.php
+++ public_html/wp-content/themes/twentyfourteen-pp/views/pods-widget-view-ann-side-kom.php
@@ -17,12 +17,8 @@
 		);
 	echo PHP_EOL, ' <ul class="', pp_ann_type(), '">';
 	if ( $qobj && $qobj->taxonomy && $qobj->taxonomy == pp_kom_tax() ) {
-//		$num_annonser = max(
-//			intval( get_terms( pp_kom_tax(),  array( 'slug'   => $qobj->slug,      'fields' => 'count' ) ) ),
-//			intval( get_terms( pp_apos_tax(), array( 'parent' => $parent->term_id, 'fields' => 'count' ) ) )
-//		);
 		$transient = pp_ann_type() . '_' . pp_side_term() . '_' . pp_kom_tax() . '_' . $qobj->slug;
-		delete_transient( $transient );
+//		delete_transient( $transient );
 		$annonser = get_transient( $transient );
 		if ( $annonser && is_array( $annonser ) && count( $annonser ) ) {
 			$source = 'transient';
@@ -31,12 +27,17 @@
 			$annonser = array();
 			for ( $apos = 0; $apos < $num_annonser; $apos++ ) {
 				$alev = 0; //Prøv først med både kommune, posisjon og prioritet
+				$parent = get_term_by( 'id', get_term_by( 'slug', $qobj->slug, $qobj->taxonomy )->parent, $qobj->taxonomy );
+				if ( false === $term )
+					$terms = $qobj->slug;
+				else
+					$terms = array( $parent->slug, $qobj->slug );
 				while( empty( $annonser[ $apos ] ) && $alev < count( $alev_terms ) ) {
 					$annonse = get_posts( array(
 						'posts_per_page' => 1,
 						'post_type' => pp_ann_type(),
 						'tax_query' => array( 'relation' => 'AND',
-							array( 'taxonomy' => $qobj->taxonomy, 'field' => 'slug', 'terms' => $qobj->slug            ),
+							array( 'taxonomy' => $qobj->taxonomy, 'field' => 'slug', 'terms' => $terms                 ),
 							array( 'taxonomy' => pp_apos_tax(),   'field' => 'slug', 'terms' => 'pos-' . ( $apos + 1 ) ),
 							array( 'taxonomy' => pp_alev_tax(),   'field' => 'slug', 'terms' => $alev_terms[ $alev ]   )
 						),
@@ -44,10 +45,6 @@
 						'exclude' => array_unique( $idsx ),
 						'orderby' => 'rand'
 					) );
-//					echo $qobj->slug, ', ';
-//					print_r( $annonse); 
-//					if( $apos==0)echo count( $annonse ), ', ';
-//					if ( $apos+1 == 1 ) {echo 'mkom ', $apos+1, ' ', $alev, ' ', ($annonse[0]->ID ? $annonse[0]->ID : 0), ' '; print_r( $idsx ); echo '<br/>';}
 					if ( count( $annonse ) ) {
 //						$annonse[0]->src = $annonse[0]->ID . ' pos-' . ( $apos + 1 ) . ' ' . $alev_terms[ $alev ] . ' ' . $qobj->name;
 						$annonser[ $apos ] = $annonse[0];
@@ -68,7 +65,6 @@
 						'exclude' => array_unique( $idsx ),
 						'orderby' => 'rand'
 					) );
-//					if ( $apos+1 == 1 ) {echo 'upopr ', $apos+1, ' ', $alev, ' ', ($annonse[0]->ID ? $annonse[0]->ID : 0), ' '; print_r( $idsx ); echo '<br/>';}
 					if ( count( $annonse ) ) {
 						$annonse[0]->src = $annonse[0]->ID . ' pos-' . ( $apos + 1 ) . ' ' . $qobj->name;
 						$annonser[ $apos ] = $annonse[0];
@@ -89,7 +85,6 @@
 						'exclude' => array_unique( $idsx ),
 						'orderby' => 'rand'
 					) );
-//					if ( $apos+1 == 1 ) {echo 'ukom ', $apos+1, ' ', $alev, ' ', ($annonse[0]->ID ? $annonse[0]->ID : 0), ' '; print_r( $idsx ); echo '<br/>';}
 					if ( count( $annonse ) ) {
 						$annonse[0]->src = $annonse[0]->ID . ' pos-' . ( $apos + 1 ) . ' ' . $alev_terms[ $alev ];
 						$annonser[ $apos ] = $annonse[0];
@@ -131,7 +126,6 @@
 						'exclude' => array_unique( $idsx ),
 						'orderby' => 'rand'
 					) );
-//					if ( $apos+1 == 1 ) {echo 'upopr ', $apos+1, ' ', $alev, ' ', ($annonse[0]->ID ? $annonse[0]->ID : 0), ' '; print_r( $idsx ); echo '<br/>';}
 					if ( count( $annonse ) ) {
 						$annonse[0]->src = $annonse[0]->ID . ' ' . $alev_terms[ $alev ] . ' ' . $qobj->name;
 						$annonser[ $apos ] = $annonse[0];
@@ -152,7 +146,6 @@
 						'exclude' => array_unique( $idsx ),
 						'orderby' => 'rand'
 					) );
-//					if ( $apos+1 == 1 ) {echo 'upopr ', $apos+1, ' ', $alev, ' ', ($annonse[0]->ID ? $annonse[0]->ID : 0), ' '; print_r( $idsx ); echo '<br/>';}
 					if ( count( $annonse ) ) {
 						$annonse[0]->src = $annonse[0]->ID . ' ' . $alev_terms[ $alev ];
 						$annonser[ $apos ] = $annonse[0];
@@ -173,7 +166,6 @@
 						'exclude' => array_unique( $idsx ),
 						'orderby' => 'rand'
 					) );
-//					if ( $apos+1 == 1 ) {echo 'upoupr ', $apos+1, ' ', $alev, ' ', ($annonse[0]->ID ? $annonse[0]->ID : 0), ' '; print_r( $idsx ); echo '<br/>';}
 					if ( count( $annonse ) ) {
 						$annonse[0]->src = $annonse[0]->ID . ' ' . $qobj->name;
 						$annonser[ $apos ] = $annonse[0];
@@ -198,13 +190,12 @@
 						$annonser[ $apos ] = $annonse[0];
 						$idsx[] = intval( $annonse[0]->ID );
 					}
-				} //print_r( $idsx ); echo '<br/>';
+				}
 			}
 			set_transient( $transient, $annonser, PP_ANN_TRANS_EXP );
 		}
 		pp_widget_annonser_li( $annonser, false, null, $source );
 	} else {
-//		$num_annonser = intval( get_terms( pp_apos_tax(), array( 'parent' => $parent->term_id, 'fields' => 'count' ) ) );
 		$transient = pp_ann_type() . '_' . pp_side_term();
 		$annonser = get_transient( $transient );
 		if ( $annonser && is_array( $annonser ) && count( $annonser ) ) {
@@ -234,14 +225,13 @@
 						$idsx[] = intval( $annonse[0]->ID );
 					}
 					$alev++;	// Fra pri-1 til pri-3 via $alev_terms
-//					echo $apos, ' ', $alev, '<br/>';
 				}
 				while( empty( $annonser[ $apos ] ) && $alev < count( $alev_terms ) ) {
 					$annonse = get_posts( array(
 						'posts_per_page' => 1,
 						'post_type' => pp_ann_type(),
 						'tax_query' => array( 'relation' => 'AND',
-//							array( 'taxonomy' => pp_kom_tax(),  'field' => 'id',   'terms' => $termsx, 'operator' => 'NOT IN', 'include_children' => false ),
+							array( 'taxonomy' => pp_kom_tax(),  'field' => 'id',   'terms' => $termsx, 'operator' => 'NOT IN', 'include_children' => false ),
 							array( 'taxonomy' => pp_apos_tax(), 'field' => 'slug', 'terms' => pp_side_term() ),
 							array( 'taxonomy' => pp_alev_tax(), 'field' => 'slug', 'terms' => $alev_terms[ $alev ]   )
 						),
